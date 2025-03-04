e5a86603e0f7281f62b9ec36b0c07e4818ee050a
inseong01 <insung940@naver.com>
2025-03-04 18:37:59 +0900

    chore: 웹뷰 개발 서버 구축
    - next 포트 접속 오류 해결
    - 웹뷰 -> 네이티브 데이터 전달 확인

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,42 @@
-import { SafeAreaView, Text } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { router } from 'expo-router';
+import { useEffect, useRef, useState } from 'react';
+import { Button, Dimensions, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { WebView, WebViewMessageEvent } from 'react-native-webview';
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+// const postMSG = () => postMessage('Hi');
 
 export default function Index() {
+  const [tableName, getTableName] = useState('');
+  const webviewRef = useRef<WebView>(null);
+  const route = '';
+
+  function onMessage(e: WebViewMessageEvent) {
+    console.log('Get Message!', e.nativeEvent.data);
+    // router.navigate('http://192.168.0.13:3000/1/bill');
+  }
+
   return (
-    <SafeAreaView>
-      <Text>dd</Text>
-      <WebView source={{ uri: '192.168.0.0:192' }} />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={webviewRef}
+        style={styles.webview}
+        source={{ uri: `http://192.168.0.13:3000/1` }}
+        onMessage={onMessage}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    width: deviceWidth,
+    height: deviceHeight,
+  },
+});
